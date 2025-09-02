aa0e7b1abeb2d7e504ec13c5a0edb8ad12ed6f15
kevindandrew <kevindandrew77@gmail.com>
2025-09-02 11:56:32 -0400

    Agregar formulario para capturar datos del usuario y mejorar la estructura del HTML

--- dia-17/main.js
+++ dia-17/main.js
@@ -1,5 +1,5 @@
 /* -----------seleecionar el elemento-------- */
-let botoncito = document.querySelector("#btn");
+/* let botoncito = document.querySelector("#btn");
 let parrafo = document.querySelector("#parrafo");
 botoncito.addEventListener("click", function () {
   if (botoncito.classList.contains("bg-blue-800")) {
@@ -8,11 +8,59 @@ botoncito.addEventListener("click", function () {
     botoncito.classList.replace("bg-red-800", "bg-blue-800");
   }
   parrafo.textContent = "funval siempre los mejores";
-});
+}); */
 /* 
     crear un elemento un boton si gustan  q represente al dia
     al darle click debera pasar a noche 
     fiona debera pasar ds ser una princesa
     a una ogro
+*/
+/* let boton = document.querySelector("#boton");
+let dia = true;
+let princesa = document.querySelector("#princesa");
+let ogro = document.querySelector("#ogro");
+let cuerpo = document.querySelector("#cuerpo");
+boton.addEventListener("click", function () {
+  dia = !dia;
+  if (dia) {
+    boton.textContent = "Dia";
+    cuerpo.classList.replace("bg-slate-800", "bg-amber-500");
+    princesa.classList.remove("hidden");
+    ogro.classList.add("hidden");
+  } else {
+    boton.textContent = "Noche";
+    cuerpo.classList.replace("bg-amber-500", "bg-slate-800");
+    ogro.classList.remove("hidden");
+    princesa.classList.add("hidden");
+  }
+}); */
 
+/* 
+{
+    nombres: "string",
+    apellidos:"string"
+}
+*/
+let form = document.querySelector("#formulario");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  //console.log(e);
+  let obj = {};
+  let nombreInput = document.querySelector("#nombre").value;
+  let apellidosInput = document.querySelector("#Apellidos").value;
+  obj.nombres = nombreInput;
+  obj.apellidos = apellidosInput;
+  console.log(nombreInput, apellidosInput);
+  console.log(obj);
+});
+
+/* 
+    pedir datos a un usuario de la forma q ustedes gusten
+    pero debera llegarme en el siguiente formato
+    {
+        nombre:"string",
+        edad:number,
+        esMiembroSud:true    
+    }
 */
